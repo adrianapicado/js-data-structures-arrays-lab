06490cda47eb0d3fa9614d36d896ecf0a9b90acb
adriana <picadoadriana7@gmail.com>
2021-08-10 14:04:42 -0400

    commit tree c1c6c189180cf848c0f395825769281b599dea8f

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+  }
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+  }
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name)
+}
+
+function appendDriver(name) {
+    const newArray = [...drivers, name];
+    return newArray;
+}
+
+function prependDriver(name) {
+    const newArray = [name, ...drivers];
+    return newArray; 
+}
+
+function removeLastDriver(name) {
+    const newArray = drivers.slice(0, -1);
+    return newArray;
+}
+
+function removeFirstDriver(name) {
+    const newArray = drivers.slice(1);
+    return newArray;
+}
